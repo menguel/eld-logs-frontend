4782cfbc28a86372371c3032a64c0d5d3a979e8e
menguel <menguelo.js@gmail.com>
2025-04-11 16:44:03 +0100

    End of first configuration

---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DashboardContainer from "./container/DashboardContrainer";
+
+const Dashboard = () => {
+    return(
+        <>
+            <DashboardContainer/>
+        </>
+    )
+};
+
+export default Dashboard;
---
+++ src/pages/form/Form.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import FormContainer from "./container/FormContainer";
+
+
+const Form = () => {
+    return(
+        <>
+            <FormContainer/>
+        </>
+    )
+}
+
+export default Form;
